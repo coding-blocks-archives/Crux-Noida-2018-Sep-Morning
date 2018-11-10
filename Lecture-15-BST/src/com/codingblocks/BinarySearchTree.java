e6d76d64cc9b4b5ba2b63de31710453b8e189af9
KeenWarrior <anujgargcse@gmail.com>
2018-11-10 12:35:12 +0530

    BST

--- Lecture-15-BST/src/com/codingblocks/BinarySearchTree.java
+++ Lecture-15-BST/src/com/codingblocks/BinarySearchTree.java
@@ -2,12 +2,31 @@ package com.codingblocks;
 
 import javafx.scene.chart.ValueAxis;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 public class BinarySearchTree <T extends Comparable<T>> {
 
     private Node root;
+
+    public void generateFromSorted(ArrayList<T> list){
+        generateFromSorted(list, 0, list.size());
+    }
+
+    private void generateFromSorted(ArrayList<T> list, int start, int end) {
+        if (start >= end){
+            return;
+        }
+
+        int mid = (start + end - 1) / 2;
+
+        insert(list.get(mid));
+
+        generateFromSorted(list, start, mid);
+        generateFromSorted(list, mid + 1, end);
+    }
 
     public void insert(T value){
         root = insert(value, root);
@@ -26,7 +45,17 @@ public class BinarySearchTree <T extends Comparable<T>> {
             node.right = insert(value, node.right);
         }
 
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+
         return node;
+    }
+
+    private int height(Node node){
+        if (node == null){
+            return -1;
+        }
+
+        return node.height;
     }
 
     public void display(){
@@ -38,7 +67,7 @@ public class BinarySearchTree <T extends Comparable<T>> {
             return;
         }
 
-        System.out.println(indent + node.value + " " + position);
+        System.out.println(indent + node.value + " " + position + " with height " + node.height);
 
         display(node.left, "left", indent + "    ");
         display(node.right, "right", indent + "    ");
@@ -83,6 +112,60 @@ public class BinarySearchTree <T extends Comparable<T>> {
         }
     }
 
+    public boolean find(T value){
+        return find(value, root);
+    }
+
+    private boolean find(T value, Node node) {
+        if (node == null){
+            return false;
+        }
+
+        if (node.value.compareTo(value) > 0){
+            return find(value, node.left);
+        }
+
+        if (node.value.compareTo(value) < 0){
+            return find(value, node.right);
+        }
+
+        return true;
+    }
+
+    public LinkedList<T> sorted(){
+        LinkedList<T> list = new LinkedList<>();
+
+        sorted(root, list);
+
+        return list;
+    }
+
+    private void sorted(Node node, LinkedList<T> list) {
+        if (node == null){
+            return;
+        }
+
+        sorted(node.left, list);
+        list.addLast(node.value);
+        sorted(node.right, list);
+    }
+
+    public boolean balanced(){
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+
+        if (Math.abs(height(node.left) - height(node.right)) > 1 ){
+            return false;
+        }
+
+        return balanced(node.left) && balanced(node.right);
+    }
+
     public void levelOrder(){
 
         if (root == null){
@@ -108,14 +191,37 @@ public class BinarySearchTree <T extends Comparable<T>> {
         }
     }
 
+    public boolean isBST(){
+        return isBST(root, null, null);
+    }
+
+    private boolean isBST(Node node, T max, T min) {
+        if (node == null){
+            return true;
+        }
+
+        if (min != null && node.value.compareTo(min) <= 0){
+            return false;
+        }
+
+        if (max != null && node.value.compareTo(max) >= 0){
+            return false;
+        }
+
+        return isBST(node.left, node.value, min) && isBST(node.right, max, node.value);
+    }
+
 
     private class Node {
         private T value;
         private Node left;
         private Node right;
 
+        private int height;
+
         public Node(T value) {
             this.value = value;
+            this.height = 0;
         }
     }
 }
--- Lecture-15-BST/src/com/codingblocks/Main.java
+++ Lecture-15-BST/src/com/codingblocks/Main.java
@@ -1,5 +1,8 @@
 package com.codingblocks;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,12 +10,35 @@ public class Main {
 
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
 
-        bst.insert(10);
-        bst.insert(5);
-        bst.insert(20);
-        bst.insert(15);
-        bst.insert(7);
+//        bst.insert(10);
+//        bst.insert(5);
+//        bst.insert(20);
+//        bst.insert(15);
+//        bst.insert(7);
 
-        bst.levelOrder();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+
+        bst.generateFromSorted(list);
+
+//        bst.levelOrder();
+
+        System.out.println(bst.isBST());
+
+        bst.display();
+
+//        LinkedList<Integer> list = bst.sorted();
+//
+//        for (Integer item : list) {
+//            System.out.println(item);
+//        }
+
     }
 }
